27e661744a987b818d61360f82b088626662961a
tkmagesh <tkmagesh77@gmail.com>
2014-05-07 16:11:16 +0530

    Functional Programming Techniques - Seed Data

---
+++ Day-1/JavaScript/FunctionalProgrammingTechniques/products.js
@@ -0,0 +1,12 @@
+var products = [
+	{id:1,name:"pen",cost:10,units:60,category:1},
+	{id:6,name:"hen",cost:40,units:40,category:2},
+	{id:3,name:"ten",cost:70,units:70,category:2},
+	{id:9,name:"len",cost:30,units:10,category:1},
+	{id:2,name:"den",cost:20,units:30,category:2},
+	{id:7,name:"zen",cost:90,units:20,category:1}
+]
+
+function sort(list){
+	/*provide the implementation to sort the list by "id"*/
+}
