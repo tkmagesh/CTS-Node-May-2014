b01d183e5c29b06de654a98f047db02ce1ab8c2a
tkmagesh <tkmagesh77@gmail.com>
2014-05-07 12:13:04 +0530

    Calculator - able to add variable length arguments

--- Day-1/JavaScript/LanguageExploration/calculator.js
+++ Day-1/JavaScript/LanguageExploration/calculator.js
@@ -1,5 +1,10 @@
 function add(x,y){
-	x = isNaN(x) ? 0 : parseInt(x);
-	y = isNaN(y) ? 0 : parseInt(y); 
-	return x + y;
+	function parseArg(n){
+		if (typeof n === "function") return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n);
+	}
+	var result = 0;
+	for(var i=0;i<arguments.length;i++)
+		result += parseArg(arguments[i]);
+	return result;
 }
--- Day-1/JavaScript/LanguageExploration/calculatorTests.js
+++ Day-1/JavaScript/LanguageExploration/calculatorTests.js
@@ -32,7 +32,43 @@ window.addEventListener("DOMContentLoaded",function(){
 		return result === expectedResult;
 	});
 	
+	test("Should be able to add functions returning numbers",function(){
+		var f1 = function(){ return 10;},
+			f2 = function(){ return 20;},
+			expectedResult = 30;
+
+		var result = add(f1,f2);
+
+		return result === expectedResult;
+	});
 	
+	test("Should be able to add functions returning numbers in string format",function(){
+		var f1 = function(){ return "10";},
+			f2 = function(){ return "20";},
+			expectedResult = 30;
+
+		var result = add(f1,f2);
+
+		return result === expectedResult;
+	});
+	
+	test("Should be able to add functions returning function returning numbers in string format",function(){
+		var f1 = function(){ return function(){ return "10";}},
+			f2 = function(){ return function(){ return "20";}},
+			expectedResult = 30;
+
+		var result = add(f1,f2);
+
+		return result === expectedResult;
+	});
+
+	test("Should be able to add variable length arguments",function(){
+		var expectedResult = 100;
+
+		var result = add(10,20,30,40);
+
+		return result === expectedResult;
+	});
 
 
 });
