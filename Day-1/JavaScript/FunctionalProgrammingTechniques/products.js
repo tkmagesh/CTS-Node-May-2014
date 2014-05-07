3e49b5f4f6666fced4f593a59d8d4e60cccd80b1
tkmagesh <tkmagesh77@gmail.com>
2014-05-07 16:39:03 +0530

    FP Assignments

--- Day-1/JavaScript/FunctionalProgrammingTechniques/products.js
+++ Day-1/JavaScript/FunctionalProgrammingTechniques/products.js
@@ -7,6 +7,39 @@ var products = [
 	{id:7,name:"zen",cost:90,units:20,category:1}
 ]
 
-function sort(list){
-	/*provide the implementation to sort the list by "id"*/
+function sort(list,attrName){
+	for(var i=0;i<=list.length-1;i++)
+		for(var j=i+1;j<list.length;j++)
+			if (list[i][attrName] > list[j][attrName]){
+				var temp = list[i];
+				list[i] = list[j];
+				list[j] = temp;
+			}
+		
 }
+
+function filter(list,criteriaFn){
+	var result = [];
+	for(var i=0;i<list.length;i++)
+		if (criteriaFn(list[i]) === true)
+			result.push(list[i]);
+	return result;
+}
+
+function costlyProductCriteria(p){
+	return p.cost > 50;
+}
+
+var costlyProducts = filter(products,costlyProductCriteria);
+
+/*Create the following functions (generalized)
+min
+max
+average
+countBy
+sum
+groupBy
+join
+transform
+
+*/
