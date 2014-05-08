9b9b2a37dc4a0cb86eadb5dd9a4f59385ba33217
tkmagesh <tkmagesh77@gmail.com>
2014-05-08 12:22:05 +0530

    Prototypal Inheritance

--- Day-2/JavaScript-Continuation/ConstructorFunction.js
+++ Day-2/JavaScript-Continuation/ConstructorFunction.js
@@ -13,3 +13,47 @@ Modify the above function to expose accessor functions that enforce the followin
 3. "salary" will not accept a value less than the current salary
 */
 
+function Employee(id,name,salary){
+	var _id = id
+		,_name = name
+		,_salary = salary;
+
+	this.id = function(){
+		return _id;
+	};
+	this.name = function(val){
+		if (typeof val === "undefined") return _name;
+		if (val !== "") _name = val;
+	};
+	this.salary = function(val){
+		if (typeof val === "undefined") return _salary;
+		if(val > _salary) _salary = val;
+	}
+}
+
+function SalaryCalculator(basic, hra, da, tax){
+	this.basic = basic;
+	this.hra = hra;
+	this.da = da;
+	this.tax = tax;
+	this.salary = 0;
+	this.calculate = function(){
+		var gross = this.basic + this.hra + this.da;
+		this.salary = gross * ((100-this.tax)/100);
+	}
+}
+
+/*Better version of the above is*/
+
+function SalaryCalculator(basic, hra, da, tax){
+	this.basic = basic;
+	this.hra = hra;
+	this.da = da;
+	this.tax = tax;
+	this.salary = 0;
+
+}
+SalaryCalculator.prototype.calculate = function(){
+	var gross = this.basic + this.hra + this.da;
+	this.salary = gross * ((100-this.tax)/100);
+}
