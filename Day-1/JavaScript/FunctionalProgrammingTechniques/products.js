e6ea3e842f337d8f5a0e65c13031b595c6dcb0b9
tkmagesh <tkmagesh77@gmail.com>
2014-05-08 10:27:16 +0530

    Assignment Solutions

--- Day-1/JavaScript/FunctionalProgrammingTechniques/products.js
+++ Day-1/JavaScript/FunctionalProgrammingTechniques/products.js
@@ -18,10 +18,10 @@ function sort(list,attrName){
 		
 }
 
-function filter(list,criteriaFn){
+function filter(list,predicate){
 	var result = [];
 	for(var i=0;i<list.length;i++)
-		if (criteriaFn(list[i]) === true)
+		if (predicate(list[i]) === true)
 			result.push(list[i]);
 	return result;
 }
@@ -48,11 +48,92 @@ transform
 
 */
 
-function join(list1, list2,.....){
-
+function min(list,selectorFn){
+	var result = selectorFn(list[0]);
+	for(var i=1;i<list.length;i++){
+		var value = selectorFn(list[i]);
+		if (value < result) result = value;
+	}
+	return result;
 }
 
-var productsWithCategory = join(products,categories,.....);
+min(products,function(p){ return p.cost;});
+
+function min(list,selectorFn){
+	var result = selectorFn(list[0]);
+		for(var i=1;i<list.length;i++){
+			var value = selectorFn(list[i]);
+			if (value > result) result = value;
+		}
+		return result;	
+}
+
+function sum(list,selectorFn){
+	var result = 0;
+	for(var i=0;i<list.length;i++){
+		var value = selectorFn(list[i]);
+		result += value;
+	}
+	return result;
+}
+
+function countBy(list,predicate){
+	var result = 0;
+	for(var i=0;i<list.length;i++)
+		if (predicate(list[i])) ++result;
+	return result;
+}
+
+countBy(products, function(p){ return p.cost > 50; });
+
+function groupBy(list,selectorFn){
+	var result = {};
+	for(var i=0;i<list.length;i++){
+		var key = selectorFn(list[i]);
+		if (typeof result[key] === "undefined") result[key] = [];
+		result[key].push(list[i]);
+	}
+	return result;
+}
+
+function groupBy(list,attrName){
+	var result = {};
+	for(var i=0;i<list.length;i++){
+		var key = list[i][attrName];
+		if (typeof result[key] === "undefined") result[key] = [];
+		result[key].push(list[i]);
+	}
+	return result;
+}
+
+function groupBy(list,selector){
+	var result = {};
+	var selectorFn = selector;
+	if (typeof selector === "string") selectorFn = function(p){ return p[selector];}
+	for(var i=0;i<list.length;i++){
+		var key = selectorFn(list[i]);
+		if (typeof result[key] === "undefined") result[key] = [];
+		result[key].push(list[i]);
+	}
+	return result;
+}
+
+function join(leftList, rightList, leftKeySelector, rightKeySelector, transformer){
+	var result=[];
+	for(var i=0;i<leftList.length;i++){
+		var leftKey = leftKeySelector(leftList[i]);
+		for(var j=0;j<rightList.length;j++){
+			var rightKey = rightKeySelector(rightList[j]);
+			if (leftKey === rightKey)
+				result.push(transformer(leftList[i],rightList[j]));
+		}
+	}
+	return result;
+}
+
+var productsWithCategoryName = join(products,categories,function(p){ return p.category;}, function(c){ return c.id }, function(p,c){
+  return { id : p.id, name : p.name, cost : p.cost, units : p.units, category : c.name};
+});
 /*productsWithCategory will be an array like the following
 [
 	{id:1,name:"pen",cost:10,units:60,category:"stationary"},
